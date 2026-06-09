d16c22e9e4f76c6044964b665d4bf9e73b9e6574
Lera <dudik0808@gmail.com>
2026-06-09 18:50:56 +0300

    паттерн outbox. добавляем товар в каталог -> сохраняем сообщение в outbox -> асинх шедулер берет n записей -> посылает их в кафку

--- catalog/src/main/kotlin/com/lera/catalog/service/GoodService.kt
+++ catalog/src/main/kotlin/com/lera/catalog/service/GoodService.kt
@@ -1,9 +1,12 @@
 package com.lera.catalog.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.lera.catalog.dto.orders.GoodsInvalidateMessage
 import com.lera.catalog.dto.orders.GoodsItem
 import com.lera.catalog.model.GoodEntity
+import com.lera.catalog.model.OutboxMessageEntity
 import com.lera.catalog.repository.GoodRepository
+import com.lera.catalog.repository.OutboxMessageRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
@@ -12,17 +15,37 @@ import java.math.BigDecimal
 @Transactional(readOnly = true)
 class GoodService(
     private val goodRepository: GoodRepository,
-    private val kafkaProducerService: KafkaProducerService
+    private val outboxMessageRepository: OutboxMessageRepository,
+    private val objectMapper: ObjectMapper
 ) {
 
     @Transactional
     fun add(name: String, description: String, price: BigDecimal, externalId: String): GoodEntity {
         val result = goodRepository.findByExternalId(externalId)?.let { existing ->
-            goodRepository.save(existing.copy(name = name, description = description, price = price))
-        } ?: goodRepository.save(GoodEntity(name = name, description = description, price = price, externalId = externalId))
+            goodRepository.save(
+                existing.copy(
+                    name = name,
+                    description = description,
+                    price = price
+                )
+            )
+        } ?: goodRepository.save(
+            GoodEntity(
+                name = name,
+                description = description,
+                price = price,
+                externalId = externalId
+            )
+        )
 
         val message = GoodsInvalidateMessage(listOf(GoodsItem(result.id!!, result.externalId)))
-        kafkaProducerService.sendInvalidateMessage(message)
+
+        outboxMessageRepository.save(
+            OutboxMessageEntity(
+                eventType = "GOODS_INVALIDATE",
+                payload = objectMapper.writeValueAsString(message)
+            )
+        )
 
         return result
     }
