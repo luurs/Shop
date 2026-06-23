3b3953e9fcb84a8de545ffd550bfe24ba27cd160
Lera <dudik0808@gmail.com>
2026-06-23 15:53:40 +0300

    тесты к crud

--- catalog/src/test/kotlin/com/lera/catalog/integration/controller/GoodControllerIT.kt
+++ catalog/src/test/kotlin/com/lera/catalog/integration/controller/GoodControllerIT.kt
@@ -1,8 +1,10 @@
 package com.lera.catalog.integration.controller
 
 import com.lera.catalog.dto.goodController.CreateGoodRequest
+import com.lera.catalog.dto.goodController.GetGoodResponse
 import com.lera.catalog.dto.goodController.GetGoodsListRequest
 import com.lera.catalog.dto.goodController.GetGoodsListResponse
+import com.lera.catalog.dto.goodController.UpdateGoodRequest
 import com.lera.catalog.integration.BaseIntegrationTest
 import com.lera.catalog.model.EventType
 import com.lera.catalog.model.GoodEntity
@@ -104,6 +106,151 @@ class GoodControllerIT : BaseIntegrationTest() {
         assertThat(good.description).isEqualTo("very tasty pizza")
         assertThat(good.price).isEqualTo(BigDecimal("115.00"))
         assertThat(good.externalId).isEqualTo("P123ZA")
+    }
+
+    @Test
+    @DisplayName("Проверка получения одного товара")
+    fun getOneGoodTest() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+
+        val response = given()
+            .contentType(ContentType.JSON)
+            .pathParam("externalId", "P123ZA")
+            .`when`()
+            .get("/goods/{externalId}")
+            .then()
+            .statusCode(200)
+            .extract()
+            .`as`(GetGoodResponse::class.java)
+
+        assertThat(response.name).isEqualTo("pizza")
+        assertThat(response.description).isEqualTo("tasty pizza")
+        assertThat(response.price).isEqualTo(BigDecimal("100.00"))
+        assertThat(response.externalId).isEqualTo("P123ZA")
+    }
+
+    @Test
+    @DisplayName("Просим товар но он не найден -> 404")
+    fun getGoodNotFoundTest() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("externalId", "PooPA")
+            .`when`()
+            .get("/goods/{externalId}")
+            .then()
+            .statusCode(404)
+            .body("message", equalTo("Good with externalId=PooPA not found"))
+    }
+
+    @Test
+    @DisplayName("Успешное обновление существующего товара")
+    fun updateGoodSuccess() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("externalId", "P123ZA")
+            .body(
+                UpdateGoodRequest(
+                    "pizza2",
+                    "very tasty pizza",
+                    BigDecimal.valueOf(111)
+                )
+            )
+            .`when`()
+            .put("/goods/{externalId}")
+            .then()
+            .statusCode(200)
+
+        val result = findAllGoods().first()
+        assertThat(result.name).isEqualTo("pizza2")
+        assertThat(result.description).isEqualTo("very tasty pizza")
+        assertThat(result.price).isEqualTo(BigDecimal("111.00"))
+        assertThat(result.externalId).isEqualTo("P123ZA")
+
+        val outbox = outboxMessageRepository.findAll().toList()
+        assertThat(outbox).hasSize(1)
+    }
+
+    @Test
+    @DisplayName("Успешное софт удаление товара: смена флага delete на true")
+    fun softDeleteGoodSuccess() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("externalId", "P123ZA")
+            .`when`()
+            .delete("/goods/{externalId}")
+            .then()
+            .statusCode(200)
+
+        val result = findAllGoods().first()
+        assertThat(result.deleted).isTrue
+
+        val outbox = outboxMessageRepository.findAll().toList()
+        assertThat(outbox).hasSize(1)
+    }
+
+    @Test
+    @DisplayName("Пагинация - две страницы по 2 товара максимум")
+    fun getAllGoodsWithPagination() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+        insertGood("apple", "tasty apple", BigDecimal.valueOf(50), "test1")
+        insertGood("banana", "tasty banana", BigDecimal.valueOf(150), "test2")
+
+        given()
+            .contentType(ContentType.JSON)
+            .queryParam("page", 0)
+            .queryParam("size", 2)
+            .`when`()
+            .get("/goods")
+            .then()
+            .statusCode(200)
+            .body("content.size()", equalTo(2))
+            .body("totalElements", equalTo(3))
+            .body("totalPages", equalTo(2))
+    }
+
+    @Test
+    @DisplayName("Сортировка по цене")
+    fun getAllGoodsSortedByPrice() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+        insertGood("apple", "tasty apple", BigDecimal.valueOf(50), "test1")
+        insertGood("banana", "tasty banana", BigDecimal.valueOf(150), "test2")
+
+        given()
+            .contentType(ContentType.JSON)
+            .queryParam("sort", "price,asc")
+            .`when`()
+            .get("/goods")
+            .then()
+            .statusCode(200)
+            .body("content[0].externalId", equalTo("test1"))
+            .body("content[1].externalId", equalTo("P123ZA"))
+            .body("content[2].externalId", equalTo("test2"))
+    }
+
+    @Test
+    @DisplayName("Вернется список только удаленных товаров")
+    fun getAllGoodsOnlyDeleted() {
+        insertGood("pizza", "tasty pizza", BigDecimal.valueOf(100), "P123ZA")
+        insertGood("apple", "tasty apple", BigDecimal.valueOf(50), "test1")
+        insertGood("banana", "tasty banana", BigDecimal.valueOf(150), "test2")
+        jdbcTemplate.update("UPDATE good SET deleted = true WHERE external_id IN ('P123ZA', 'test1')")
+
+        given()
+            .contentType(ContentType.JSON)
+            .queryParam("deleted", true)
+            .`when`()
+            .get("/goods")
+            .then()
+            .statusCode(200)
+            .body("totalElements", equalTo(2))
+            .body("content[0].externalId", equalTo("P123ZA"))
+            .body("content[1].externalId", equalTo("test1"))
     }
 
     private fun insertGood(name: String, description: String, price: Number, externalId: String) {
